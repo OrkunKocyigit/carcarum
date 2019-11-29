c2268aac0db4e41fc110d9f2fc118ec8696e41b3
Orkun Koçyiğit <orkunkocyigit@gmx.com>
2019-11-29 13:03:51 +0300

    Implemented vue analytics

--- src/main.js
+++ src/main.js
@@ -4,8 +4,13 @@ import router from './router'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import VueAnalytics from 'vue-analytics'
 
 Vue.use(BootstrapVue)
+Vue.use(VueAnalytics, {
+  id: 'UA-153603466-1',
+  router
+})
 Vue.config.productionTip = false
 
 new Vue({
